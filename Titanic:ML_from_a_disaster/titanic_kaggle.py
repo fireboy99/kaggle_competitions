a7f9f8d32a34a0e4a018d94569991e9b127801fc
fireboy99 <sachinpuranik99@gmail.com>
2018-01-15 19:56:33 -0600

    Updated with dict

--- Titanic:ML_from_a_disaster/titanic_kaggle.py
+++ Titanic:ML_from_a_disaster/titanic_kaggle.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python2.7
+from __future__ import print_function
+import csv 
+
 def parse_xls(input_file):
     file_handle = open(input_file,'r')
     file_data = file_handle.read()
     file_data = file_data.split("\r\n")
     
     parsed_data = []
-    for item in file_data:
-        item = item.split(",")
+    for item in csv.reader(file_data, delimiter=","):
+    #    print item 
+    #for item in file_data:
+    #    item = item.split(",")
         parsed_data.append(item)
     return parsed_data        
 
@@ -15,17 +20,27 @@ def list_to_feature(input_list):
     header = input_list[0]
 
     for index,feature in enumerate(header):
-        feature_list[feature] = input_list[1][index]
-        print feature, input_list[1][index] 
-    print feature_list
+        feature_list[feature] = [] 
 
-    input_list = input_list[2:len(input_list)]
+    input_list = input_list[1:len(input_list)-1]
     for item in input_list:
         for index,feature in enumerate(header):
             feature_list[feature].append(item[index])
     return feature_list       
 
+def empty_elements(input_list):
+    count = 0
+    for item in input_list:
+        if (not item):
+            count += 1
+    return count
+
+
+
 file_name = 'train.csv'
 input_lst_as_items = parse_xls(file_name)
 input_lst_as_features = list_to_feature(input_lst_as_items)
-#print(input_lst_as_items[0][0])
+for key in input_lst_as_features:
+    print (key, end='')
+    print (':', end='')
+    print(empty_elements(input_lst_as_features[key]))
