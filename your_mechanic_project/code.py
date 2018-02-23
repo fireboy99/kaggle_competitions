e10eebf6bd2bdc99e9da11a9048a04489d516b34
sachinpuranik99 <sachinpuranik99@gmail.com>
2018-02-23 02:47:14 -0600

    various bug fixes

--- your_mechanic_project/code.py
+++ your_mechanic_project/code.py
@@ -13,7 +13,7 @@ class TimeTable(object):
     head = None
     tail = None
  
-    def add(self, time_slot,connected):
+    def add(self, time_slot, connected):
         new_node = Time(time_slot, connected, None, None)
         # If no elements are present then initialize the system
         if self.head is None:
@@ -36,7 +36,7 @@ class TimeTable(object):
                 #We need to insert the new node at an appropriate position or
                 #at the end
                 current_node = self.head
-                while(current_node.next is not None):
+                while(current_node is not None):
                     if(time_slot > current_node.time_slot):
                         current_node = current_node.next
                     elif(time_slot == current_node.time_slot):
@@ -58,65 +58,49 @@ class TimeTable(object):
  
         while current_node is not None:
             if current_node.time_slot == time_slot:
-                # if it's not the first element
-                #if current_node.prev is not None:
                 current_node.connected = False
-#                 else:
-#                     # otherwise we have no prev (it's None), head is the next one, and prev becomes None
-#                     self.head = current_node.next
-#                     current_node.next.prev = None
             current_node = current_node.next
  
     def show(self):
         current_node = self.head
-        print "[",
+        final_print_str = "["
         while current_node is not None:
             if(current_node.connected == True):
                 if(current_node.prev is None):
-                    print "[",
-                    print  current_node.time_slot,
-                    print ",",
+                    final_print_str += "["
+                    final_print_str += str(current_node.time_slot)
+                    final_print_str += ", "
                 elif(current_node.prev.connected == False):
-                    print "[",
-                    print  current_node.time_slot,
-                    print ",",
+                    final_print_str += "["
+                    final_print_str += str(current_node.time_slot)
+                    final_print_str += ", "
                 elif(current_node.next == None):
-                    print  current_node.time_slot,
-                    print "]",
+                    final_print_str += str(current_node.time_slot)
+                    final_print_str += "], "
             elif((current_node.connected == False) and (current_node.prev.connected == True)):
-                print  current_node.time_slot,
-                print "]",
+                final_print_str += str(current_node.time_slot)
+                final_print_str += "], "
             current_node = current_node.next
-        print "]"
+        final_print_str = final_print_str[0:-2] + "]"
+        print final_print_str
 
 
-# In[80]:
-
-
-lines = ["add, 1, 5","remove, 2, 3", "add, 6, 8", "remove, 4, 7", "add, 2, 7"]
-
-
-# In[81]:
-
-
-print lines
-
-
-# In[83]:
-
+#lines = ["add, 1, 5","remove, 2, 3", "add, 6, 8", "remove, 4, 7", "add, 2, 7"]
+#
+#print lines
 
 table = TimeTable()
 print "Printing the TimeTable:"
-for line in lines:
-    cmd = line.replace(" ","")
-    cmd = cmd.split(",")
-    if(cmd[0] == 'add'):
-        for val in range(int(cmd[1]), int(cmd[2])):
-            table.add(val,True)
-            #print val
-        table.add(val+1,False)
-        table.show()
-    elif(cmd[0] == 'remove'):
-        for val in range(int(cmd[1]), int(cmd[2])):
-            table.remove(val)
-        table.show()
+with open("file.csv") as f:
+    for line in f:
+        cmd = line.replace(" ","")
+        cmd = cmd.split(",")
+        if(cmd[0] == 'add'):
+            for val in range(int(cmd[1]), int(cmd[2])):
+                table.add(val,True)
+            table.add(val+1,False)
+            table.show()
+        elif(cmd[0] == 'remove'):
+            for val in range(int(cmd[1]), int(cmd[2])):
+                table.remove(val)
+            table.show()
